add069149c4e0c70b18febd1de081dde30658b86
Himanshu-bhadu <himanshubhadu103@gmail.com>
2026-05-20 22:37:03 +0530

    Connection model added

---
+++ Backend/models/Connection.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const connectionSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'declined'],
+    default: 'pending'
+  }
+}, { timestamps: true });
+
+connectionSchema.index({ sender: 1, recipient: 1 }, { unique: true });
+
+module.exports = mongoose.model('Connection', connectionSchema);
