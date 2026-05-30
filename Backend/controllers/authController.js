037f325d9fdd82251c80a19fa27a920072fa35c3
Himanshu-bhadu <himanshubhadu103@gmail.com>
2026-05-31 00:11:06 +0530

    Final Fix

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -25,8 +25,8 @@ const registerUser = async (req, res) => {
 
             res.cookie('token', token, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'Lax', 
+                secure: true,
+                sameSite: 'None', 
                 maxAge: 30 * 24 * 60 * 60 * 1000 
             });
 
